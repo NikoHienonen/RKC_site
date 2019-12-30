afecebc38ef319ae18ec800306aa65c42903cb35
Niko Hienonen <niko.hienonen@cs.tamk.fi>
2019-12-30 19:13:13 +0200

    deploy mod

--- index.js
+++ index.js
@@ -46,10 +46,10 @@ app.use('/api/admin', admin);
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('frontend/build'));
+  app.use(express.static('build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
 }
 
