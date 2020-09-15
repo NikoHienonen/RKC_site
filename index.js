4cc196df8cd2c83914fc80818010f808af80c8c8
Niko Hienonen <niko.hienonen@cybercom.com>
2020-09-15 12:01:53 +0300

    console log

--- index.js
+++ index.js
@@ -1,59 +1,62 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
 //load routes
-const tournaments = require('./routes/api/Tournaments');
-const admin = require('./routes/api/Admin');
+const tournaments = require("./routes/api/Tournaments");
+const admin = require("./routes/api/Admin");
 
 //Map golbal promidr - get rid off deprication warning
 mongoose.Promise = global.Promise;
 
 // static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //body-parser middleware
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Avoid mongoose deprecation
-mongoose.set('useFindAndModify', false);
+mongoose.set("useFindAndModify", false);
 
 //DB configuration
-const db = require('./conf/keys').mongoURI;
+const db = require("./conf/keys").mongoURI;
+
+console.log(db);
 
 // connect Mongo
-mongoose.connect(db, {
-  useNewUrlParser: true
-})
-  .then(() => console.log('MongoDB connected.'))
-  .catch(err => console.log(err));
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("MongoDB connected."))
+  .catch((err) => console.log(err));
 
 // connect mongoose
-mongoose.connect(db, {
-  useNewUrlParser: true
-})
-  .then(result => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+  })
+  .then((result) => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 //Use routes
-app.use('/api/tournaments', tournaments);
-app.use('/api/admin', admin);
+app.use("/api/tournaments", tournaments);
+app.use("/api/admin", admin);
 
 // Serve static assets if in production
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static('build'));
+  app.use(express.static("build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
-
