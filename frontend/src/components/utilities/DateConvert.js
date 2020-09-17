fdbb9b7be9621bd94e485098500ca066d8ee7e63
Niko Hienonen <niko.hienonen@cybercom.com>
2020-09-17 21:19:00 +0300

    changed date bug

--- frontend/src/components/utilities/DateConvert.js
+++ frontend/src/components/utilities/DateConvert.js
@@ -1,18 +1,29 @@
 export default function DateConvert(dateString) {
-  const getDay = day => {
-    switch(day) {
-      case 1: return 'Ma';
-      case 2: return 'Ti';
-      case 3: return 'Ke';
-      case 4: return 'To';
-      case 5: return 'Pe';
-      case 6: return 'La';
-      case 7: return 'Su';   
-      default: return '';
+  const getDay = (day) => {
+    console.log(day);
+    switch (day) {
+      case 1:
+        return "Ma, ";
+      case 2:
+        return "Ti, ";
+      case 3:
+        return "Ke, ";
+      case 4:
+        return "To, ";
+      case 5:
+        return "Pe, ";
+      case 6:
+        return "La, ";
+      case 7:
+        return "Su, ";
+      default:
+        return "";
     }
-  }
-  const january = month => month === 0 ? 1 : month;
-  const  minutes = minutes => minutes === 0 ? '00' : minutes;
+  };
+  const january = (month) => (month === 0 ? 1 : month);
+  const minutes = (minutes) => (minutes === 0 ? "00" : minutes);
   const date = new Date(dateString);
-  return `${getDay(date.getDay())}, ${date.getDate()}/${january(date.getMonth())}/${date.getFullYear()} ${date.getHours()}:${minutes(date.getMinutes())}`; 
+  return `${getDay(date.getDay())}${date.getDate()}/${january(
+    date.getMonth()
+  )}/${date.getFullYear()} ${date.getHours()}:${minutes(date.getMinutes())}`;
 }
